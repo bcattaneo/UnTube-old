423713f321b576ddfe48099a1384ef46ba06fa84
mafiasud0 <sud0@unitedhack.com>
2012-12-09 13:46:18 -0300

    Fixted some things after review.

--- source/chrome/content/overlay.js
+++ source/chrome/content/overlay.js
@@ -2,15 +2,8 @@ var UnTube = {
 	onLoad: function() {
 		this.initialized = true;
 	},
-	onChangeLog: function() {
-		window.open(
-			"chrome://untube/content/ChangeLog",
-			"ChangeLog",
-			"width=420,height=230,resizable,scrollbars=yes,status=1"
-		);
-	},
 	onsud0: function() {
-		var myUrl = "http://addons.mozilla.org/es/firefox/user/6742344/";
+		var myUrl = "http://addons.mozilla.org/firefox/user/6742344/";
 		var tBrowser = top.document.getElementById("content");
 		var tab = tBrowser.addTab(myUrl);
 		tBrowser.selectedTab = tab;
